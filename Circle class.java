69f39f42b4006cf62e8a31fcd6fc3ed1c11f42c4
imad3100 <95374795+imad3100@users.noreply.github.com>
2022-06-01 23:32:00 +0100

    Update Circle class.java

--- Circle class.java
+++ Circle class.java
@@ -1,3 +1,32 @@
+ package test;
+
+public class point {
+static private double x;
+static private double y;
+point(){
+x=0;
+y=0;
+
+}
+point(float x,float y){
+this.x=x;
+this.y=y;
+
+}
+public void move(float dx ,float dy) {
+x=x+dx;
+y=y+dy;
+
+}
+
+public double Distance(point p1,point p2) {
+
+return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x)
++ (p1.y-p2.y)*(p1.y-p2.y));
+}
+
+}
+
 public class Circle {
 	private point center ;
 	private double radius ;
